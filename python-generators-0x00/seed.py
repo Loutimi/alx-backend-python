97363ff9f778a2eecab064d232b6655cc8a66e4f
Loutimi <loutimi59@gmail.com>
2025-07-05 13:07:29 +0100

    Create python-generators-0x00/ directory

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,8 @@
+"""
+Set up the MySQL database, ALX_prodev with the table user_data with the following fields:
+user_id(Primary Key, UUID, Indexed)
+name (VARCHAR, NOT NULL)
+email (VARCHAR, NOT NULL)
+age (DECIMAL,NOT NULL)
+"""
+
