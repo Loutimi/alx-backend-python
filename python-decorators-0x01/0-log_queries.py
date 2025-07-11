108b389f80e0ae70bc4c16840ac1d1c40f776bbc
Loutimi <loutimi59@gmail.com>
2025-07-11 14:24:37 +0100

    Create log_queries function to log SQL queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,27 @@
+import sqlite3
+import functools
+from datetime import datetime  
+
+# Decorator to log SQL queries with timestamps
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        log = f"{datetime.now()} — Calling {func.__name__} with args: {args}, kwargs: {kwargs}\n"
+        log += "Query is logged in\n\n"  
+        with open("query_log.txt", "a") as f:
+            f.write(log)
+
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
