7a8437eecd76a9015dbd68795007e4f1fbff55d3
Loutimi <loutimi59@gmail.com>
2025-07-15 23:47:45 +0100

    Add test_access_nested_map_exception method that uses the assertRaises context manager to test that expected errors are raised for the defined inputs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
-
+"""A module for testing the utils module.
+"""
 import unittest
-from nose.tools import assert_equal
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from nose.tools import assert_equal
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
+
 
 class TestAccessNestedMap(unittest.TestCase):
-     """Tests the access_nested_map function."""
-     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-     ])
-     def test_access_nested_map(self, map, key_path, expected_value):
-          """Tests access_nested_map's output."""
-          assert_equal(access_nested_map(map, key_path), expected_value)
+    """Tests the access_nested_map function."""
+    @parameterized.expand([
+    ({"a": 1}, ("a",), 1),
+    ({"a": {"b": 2}}, ("a",), {"b": 2}),
+    ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, key_path, expected_value):
+        """Tests access_nested_map's output."""
+        assert_equal(access_nested_map(nested_map, key_path), expected_value)
 
 
-if __name__ == "__main__":
-     unittest.main()
+    @parameterized.expand([
+    ({}, ("a",), KeyError),
+    ({"a": 1}, ("a", "b"), TypeError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, key_path, expected_exception):
+        with self.assertRaises(expected_exception):
+            access_nested_map(nested_map, key_path)
+    
+
