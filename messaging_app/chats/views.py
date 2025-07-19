43ec0fcda253f7fe8481d8a172c5a30cf861ccf5
Loutimi <loutimi59@gmail.com>
2025-07-20 00:39:52 +0100

    Modify API endpoints for conversations and messages

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters  
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend  
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -10,13 +10,13 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]  
+    ordering_fields = ['created_at']
 
     def get_queryset(self):
-        # Only show conversations the user is part of
         return self.queryset.filter(participants=self.request.user)
 
     def perform_create(self, serializer):
-        # Automatically include the requesting user as a participant
         participants = self.request.data.get('participant_ids', [])
         if self.request.user.id not in participants:
             participants.append(self.request.user.id)
@@ -27,13 +27,12 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]  
+    filterset_fields = ['conversation']
+    ordering_fields = ['sent_at']
 
     def get_queryset(self):
-        # Filter messages by conversation if conversation_id is in query params
-        conversation_id = self.request.query_params.get('conversation_id')
-        if conversation_id:
-            return self.queryset.filter(conversation_id=conversation_id)
-        return self.queryset.none()  # prevent exposing all messages
+        return self.queryset.filter(conversation__participants=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(sender_id=self.request.user)
