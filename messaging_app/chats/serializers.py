445493b59a16188ce591b2f3e5f8ac601f7e40e4
Loutimi <loutimi59@gmail.com>
2025-07-19 19:56:13 +0100

    Add User, Conversation, Message serializers to serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,24 +4,34 @@ from .models import User, Conversation, Message
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = [
+            'user_id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'role',
+            'created_at'
+        ]
 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(source='sender_id', read_only=True)
+    sender_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), write_only=True
+    )
 
     class Meta:
         model = Message
-        fields = [
-            'message_id',
-            'sender',
-            'message_body',
-            'sent_at',
-        ]
+        fields = ['message_id', 'sender', 'sender_id', 'conversation', 'message_body', 'sent_at']
 
 
 class ConversationSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(source='participants_id', read_only=True)
+    participants = UserSerializer(many=True, read_only=True)
+    participant_ids = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), many=True, write_only=True
+    )
     messages = MessageSerializer(many=True, read_only=True)
 
     class Meta:
@@ -29,6 +39,13 @@ class ConversationSerializer(serializers.ModelSerializer):
         fields = [
             'conversation_id',
             'participants',
+            'participant_ids',
             'created_at',
             'messages',
         ]
+
+    def create(self, validated_data):
+        participant_ids = validated_data.pop('participant_ids')
+        conversation = Conversation.objects.create(**validated_data)
+        conversation.participants.set(participant_ids)
+        return conversation
