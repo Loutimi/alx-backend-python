82182a78c662530846f721c73897fcb309d482cb
Loutimi <loutimi59@gmail.com>
2025-07-23 16:47:16 +0100

    Add login and logout url paths

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
+from .auth import LoginView, LogoutView
+
 
 # Base router
 router = routers.DefaultRouter()
@@ -13,4 +15,6 @@ conversations_router.register(r'messages', MessageViewSet, basename='conversatio
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(conversations_router.urls)),
+    path('login/', LoginView.as_view(), name='token_obtain_pair'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
