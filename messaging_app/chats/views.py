d6b1298d3264866597681b8448e955ad56553c00
Loutimi <loutimi59@gmail.com>
2025-07-23 16:23:58 +0100

    Modify views.py to update both ConversationViewSet and MessageViewSet to use the custom permission

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,38 +1,43 @@
 from rest_framework import viewsets, status, filters  
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend  
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
-
+from .permissions import IsParticipant
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipant]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]  
     ordering_fields = ['created_at']
 
     def get_queryset(self):
-        return self.queryset.filter(participants=self.request.user)
+        # Only return conversations the user is part of
+        return Conversation.objects.filter(participants=self.request.user)
 
     def perform_create(self, serializer):
         participants = self.request.data.get('participant_ids', [])
         if self.request.user.id not in participants:
             participants.append(self.request.user.id)
-        serializer.save()
+        conversation = serializer.save()
+        conversation.participants.set(participants)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipant]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]  
     filterset_fields = ['conversation']
     ordering_fields = ['sent_at']
 
     def get_queryset(self):
-        return self.queryset.filter(conversation__participants=self.request.user)
+        # Only messages from conversations the user is part of
+        return Message.objects.filter(conversation__participants=self.request.user)
 
     def perform_create(self, serializer):
+        conversation = serializer.validated_data['conversation']
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied("You are not a participant in this conversation.")
         serializer.save(sender_id=self.request.user)
