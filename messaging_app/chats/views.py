05374123830aac1d41684bfa79d3624e94473319
Loutimi <loutimi59@gmail.com>
2025-07-23 17:13:00 +0100

    Modify views to implement custom permissions

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,27 +1,47 @@
-from rest_framework import viewsets, status, filters  
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend  
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsParticipantOfConversation
 
+
 class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all() 
     serializer_class = ConversationSerializer
-    permission_classes = [IsParticipantOfConversation]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]  
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering_fields = ['created_at']
 
     def get_queryset(self):
-        # Show only conversations where the user is a participant
         return self.queryset.filter(participants=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(participants=[self.request.user])  # auto-add creator
+        serializer.save(participants=[self.request.user])
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().partial_update(request, *args, **kwargs)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [IsParticipantOfConversation]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
         conversation_id = self.kwargs.get("conversation_pk")
@@ -30,3 +50,21 @@ class MessageViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         conversation_id = self.kwargs.get("conversation_pk")
         serializer.save(sender=self.request.user, conversation_id=conversation_id)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.conversation.participants.all():
+            return Response({'detail': 'Forbidden'}, status=status.HTTP_403_FORBIDDEN)
+        return super().partial_update(request, *args, **kwargs)
