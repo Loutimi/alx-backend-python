6f00232a9abdd7bc1e1d1a6a132fa28d7aa0cc62
Loutimi <loutimi59@gmail.com>
2025-07-26 17:18:33 +0100

    Add RestrictAccessByTimeMiddleware to check the current server time and deny access if request not between 6pm to 9pm

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+from datetime import datetime, time
 import logging
+from django.http import JsonResponse
+
 
 logger = logging.getLogger('user_requests')
 
@@ -15,4 +17,21 @@ class RequestLoggingMiddleware:
 
         logger.info(f"[{timestamp}] User: {user} - Path: {path}")
 
+        return self.get_response(request)
+
+
+class RestrictAccessByTimeMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_time = datetime.now().time()
+
+        # Access window: 6 PM to 9 PM
+        start_time = time(18, 0)  # 6:00 PM
+        end_time = time(21, 0)    # 9:00 PM
+
+        if not (start_time <= current_time <= end_time):
+            return JsonResponse({'error': 'Access is only allowed between 6 PM and 9 PM'}, status=403)
         return self.get_response(request)
