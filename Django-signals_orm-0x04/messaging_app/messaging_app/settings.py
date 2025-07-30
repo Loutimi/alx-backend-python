dde66339f86e123f27c73d7cfdeb89c984ed2acd
Loutimi <loutimi59@gmail.com>
2025-07-30 18:34:53 +0100

    Configure caching in settings.py

--- Django-signals_orm-0x04/messaging_app/messaging_app/settings.py
+++ Django-signals_orm-0x04/messaging_app/messaging_app/settings.py
@@ -126,3 +126,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'dashboard'      # After login, go to dashboard
 LOGOUT_REDIRECT_URL = 'login'        # After logout, go to login page
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
