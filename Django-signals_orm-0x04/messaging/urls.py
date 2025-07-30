86ca9ee9c554c01f70b71ea4984421e055e37ec7
Loutimi <loutimi59@gmail.com>
2025-07-30 16:55:33 +0100

    Create app url paths for the login, dashboard, home, and delete_user views

---
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import homepage, dashboard
+
+urlpatterns = [
+    path('', homepage, name='homepage'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+]
