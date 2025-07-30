3389bfbb6121dcf0364bc9be63a501af90901c76
Loutimi <loutimi59@gmail.com>
2025-07-30 14:42:47 +0100

    Create the Message and Notification models

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.username} sent a message to {self.receiver.username} at {self.timestamp}"
+
+
+class Notification(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='notifications')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification to {self.receiver.username} - Read: {self.is_read}"
